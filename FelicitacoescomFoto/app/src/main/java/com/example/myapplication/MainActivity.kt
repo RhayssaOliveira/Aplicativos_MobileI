8cb21ffacd404460629d17d556fdcd2fdefbf9ef
RhayssaOliveira <rhayssaosantos05@icloud.com>
2024-03-13 14:18:11 -0300

    Meu primeiro App com foto

---
+++ FelicitacoescomFoto/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.tecnologia),
+            contentDescription = "Imagem de tecnologia",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "2° Novotec Desenvolvimento de Sistemas AMS",
+        )
+    }
+}
